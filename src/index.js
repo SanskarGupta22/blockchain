27bdfeadba2735a739e0bec1b5951e65a4c57c51
SanskarGupta22 <sanskarguptagames@gmail.com>
2023-12-22 09:24:24 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Import the necessary React libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a functional component named App
+const App = () => {
+  return (
+    <div>
+      <h1 align="center">Hello, Aman bhaiya</h1>
+    </div>
+  );
+};
+
+// Render the App component inside the root element in the public/index.html file
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
